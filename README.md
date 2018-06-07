7c591c3c2e6ab22f23942b50d90467185b5645b2
restwzeasy <pdurcek@gmail.com>
2018-06-07 14:29:04 -0400

    Adding script and readme documentation.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# video_organizer_scripts
-Helpful scripts for organizing Sony video files into subfolders corresponding to the video file creation date.
+# Sony Video File Organizer Utility
+
+If you don't do regular/daily imports of your files and happen to find yourself
+in a situation where you have multiple days/weeks/months of Sony video (*.MTS, *.M2TS, *.MODD, *.MOFF)
+files within the same folder; use this utility to organize the files according to the date of the video.
+
+The script loops through the specified source folder and examines all supported Sony video files.  For each file,
+the script attempts to extract the file creation date by examining (preferably) the file name and in case of failure,
+the file creation date.  The script then checks the target location to see if a directory corresponding to the file
+creation date exists.  _(Note: Destination folders use YYYY-MM-DD format.)_  If a destination subfolder corresponding
+to the creation date doesn't exist, the script will attempt to create one and then move the file to that location.
+
+Files such as JPGs that are not supported for the organization task are skipped. 
+
+### Limitations:
+- Currently, only Mac based file processing is supported.  The datestamp extraction logic would have to be expanded in
+order to support Windows and Linux systems and at this time I simply didn't have a need to do that.  A great example on
+the web (https://stackoverflow.com/questions/237079/how-to-get-file-creation-modification-date-times-in-python) should
+be handy for expanding the functionality.
+- I haven't done heavy testing of file conflicts as I'm usually specifying a new destination folder
+and am dealing with merging to existing folder manually.  More complex case testing is a TODO for future.
+
+###Requirements:
+- The file has to have one of 2 ways to identify the corresponding timestamp.
+    - either the video has to use a datestamp filename like 20180902142357.m2ts
+    - or, the file has to have valid/original timestamp such as file 00785.MTS corresponding to datestamp of 2018-03-12
+     and some time.
+- To deal with directory creation permissions, the script has to be executed via _sudo_.
+  
+## Usage:
+
+```pauls-imac:video_organizer_scripts pauld$ sudo ./findAndOrganizeVideos.py -s /Volumes/Photos/photos_to_sort/2011/2011-12 -t /Volumes/Multimedia/photos2/sorted_videos/```
+
+where
+- _-s_ corresponds to the source folder that contains the videos to organise
+- _-t_ corresponds to the target folder where sorted video folders with videos should be placed
---
+++ findAndOrganizeVideos.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+
+import os, sys, getopt, errno, shutil
+# import fnmatch # file name matching
+import re # regular expressions
+import platform
+import datetime # computing datestamp from file birthtime
+
+def main(argv):
+    sourcepath = ''
+    dstpath = ''
+
+    if platform.system() == 'Windows':
+        print "Currently don't support Windows based systems due to file datestamp processing complexities.  Please use a Mac."
+        sys.exit(2)
+
+    try:
+        opts, args = getopt.getopt(argv, "hs:t:", ["src=","dst="])
+    except getopt.GetoptError:
+        print('findAndOrganizeVideos.py -s <directory_with_movies> -t <destination_directory>')
+        print "Example:"
+        print "        ./findAndOrganizeVideos.py -s /Volumes/Photos/photos_to_sort/2011/2011-08-30_Europe-2011_b -t /Volumes/Multimedia/photos2/sorted_videos/"
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'findAndOrganizeVideos.py -s <directory_with_movies> -t <destination_directory>'
+            print "Example:"
+            print "       ./findAndOrganizeVideos.py -s /Volumes/Photos/photos_to_sort/2011/2011-08-30_Europe-2011_b -t /Volumes/Multimedia/photos2/sorted_videos/"
+            sys.exit()
+        elif opt in ("-s", "-src"):
+            sourcepath = arg
+        elif opt in ("-t", "-dst"):
+            dstpath = arg
+
+    if sourcepath == "":
+        print "Missing required parameter -s. Exiting."
+        sys.exit(2)
+    elif dstpath == "":
+        print "Missing required parameter -t. Exiting."
+        sys.exit(2)
+
+    print "Scanning directory", sourcepath, "for video files to be moved to", dstpath
+    scandir(sourcepath, dstpath)
+
+
+def scandir( mypath, dstpath ):
+    for root, dirs, files in os.walk(mypath):
+        for file in files:
+            if (file.endswith(".m2ts") or file.endswith(".modd") or file.endswith(".moff") or file.endswith(".MTS")):
+                print "processing filename:", file
+                fullSourcePath = os.path.join(root, file)
+                targetDir = computeDatastamp(file, fullSourcePath)
+
+                fullTargetDir = dstpath + targetDir
+                # print "fullTargetDir ===", fullTargetDir
+
+                fullTargetPath = os.path.join(fullTargetDir, file)
+
+                createdir( fullTargetDir )
+                movefile( fullSourcePath, fullTargetPath )
+            else:
+                print "Skipping unrecognized filename", file
+    return;
+
+# compute datestamp
+def computeDatastamp( file, fullSourcePath ):
+    datestampPat = re.compile('[0-9]{8}')
+    # if (fnmatch.fnmatch(file, datestampPat)):
+    if datestampPat.search(file) is not None:
+        # print "Processing like a file with datestamp name."
+        datestamp=file[:8]
+        year=datestamp[:4]
+        month=datestamp[4:6]
+        day=datestamp[6:8]
+        targetdir=year + "-" + month + "-" + day
+    else:
+        # need to get datestamp from created date
+        stat = os.stat(fullSourcePath)
+        try:
+            datestamp = datetime.date.fromtimestamp( stat.st_birthtime )
+            print "Will use file birthtime as datestamp."
+        except AttributeError:
+            datestamp = datetime.date.fromtimestamp( stat.st_mtime )
+            print "Will make datestamp equivalent to last modification date."
+        print "using datestamp", datestamp
+        targetdir = datestamp.isoformat()
+
+    return targetdir;
+
+
+def createdir( fulltargetpath ):
+    if not os.path.exists(fulltargetpath):
+        print "Creating directory ", fulltargetpath
+        try:
+            os.makedirs(fulltargetpath)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    return;
+
+
+def movefile( source, destination ):
+    try:
+        print "moving", source, "to", destination
+        shutil.move(source, destination)
+        # os.rename(source, destination)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    return;
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
