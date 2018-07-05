ac6cb5c9eeb0b87461b5dead0da63ad55bb4a226
restwzeasy <pdurcek@gmail.com>
2018-07-05 02:24:09 -0400

    Fixing findMissing script - getting it to start and try to execute.  Actually printing sha1 values for files.  WIP.

--- findMissing.py
+++ findMissing.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import psycopg2
+import os, sys, getopt, errno
 import platform, sys, hashlib
 
 BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
@@ -10,21 +11,21 @@ def main(argv):
     comppath = ''
 
     if platform.system() == 'Windows':
-        print "Currently don't support Windows based systems due to file datestamp processing complexities.  Please use a Mac."
+        print("Currently don't support Windows based systems due to file datestamp processing complexities.  Please use a Mac.")
         sys.exit(2)
 
     try:
         opts, args = getopt.getopt(argv, "hs:c:", ["org=","comp="])
     except getopt.GetoptError:
         print('findMissing.py -s <original_directory_with_files> -c <comparison_directory_with_files>')
-        print "Example:"
-        print "        ./findMissing.py -s /Volumes/Photos/ -c /Volumes/Multimedia/photos2/"
+        print("Example:")
+        print("        ./findMissing.py -s /Volumes/Photos/ -c /Volumes/Multimedia/photos2/")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print './findMissing.py -s <original_directory_with_files> -c <comparison_directory_with_files>'
-            print "Example:"
-            print "       ./findMissing.py -s /Volumes/Photos/ -c /Volumes/Multimedia/photos2/"
+            print('./findMissing.py -s <original_directory_with_files> -c <comparison_directory_with_files>')
+            print("Example:")
+            print("       ./findMissing.py -s /Volumes/Photos/ -c /Volumes/Multimedia/photos2/")
             sys.exit()
         elif opt in ("-s", "-org"):
             sourcepath = arg
@@ -32,16 +33,18 @@ def main(argv):
             comppath = arg
 
     if sourcepath == "":
-        print "Missing required parameter -s. Exiting."
+        print("Missing required parameter -s. Exiting.")
         sys.exit(2)
     elif comppath == "":
-        print "Missing required parameter -c. Exiting."
+        print("Missing required parameter -c. Exiting.")
         sys.exit(2)
 
-    print "Scanning directory", sourcepath, " and comparing vs files in", comppath
+    print("Scanning directory", sourcepath, " and comparing vs files in", comppath)
+    scandir(sourcepath)
+    scandir(comppath)
 
 
-def scandir():
+def scandir(mypath):
     for root, dirs, files in os.walk(mypath):
         for file in files:
             fullSourcePath = os.path.join(root, file)
@@ -56,4 +59,8 @@ def createHash( bfile ):
             if not data:
                 break
             sha1.update(data)
-    print "file: ", bfile, "sha1: ", sha1
+    print("file: " + bfile, "sha1: ", sha1.hexdigest())
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
