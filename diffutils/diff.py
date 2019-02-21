a866fddaa1b10601371627e63cde08b1fc639a1c
restwzeasy <pdurcek@gmail.com>
2019-02-20 22:25:22 -0500

    Enhancing missing file processing - working version.

--- diffutils/diff.py
+++ diffutils/diff.py
@@ -4,7 +4,13 @@
 # Logic for computing missing and duplicate files using Postgresql DB.
 #
 
+import os
+
+from shutil import copy
 from dbutils import required as db
+
+MISSING_FROM_COMP_FOLDER = "missing_from_comp"
+MISSING_FROM_SRC_FOLDER = "missing_from_source"
 
 def handleDuplicates(conn):
     # conn = db.getConnection(dbName)
@@ -19,17 +25,41 @@ def handleDuplicates(conn):
         # conn.close
         db.returnConnection(conn)
 
+#######################
+# Logic for identifying missing files in the source and destination/comparison locations.
+#######################
 def handleMissing(sourceSchema, compSchema):
     # conn = db.getConnection(dbName)
     conn = db.getConnection()
     cur = conn.cursor()
     try:
+        # identify missing files from the comparison location
         command = "SELECT * from %s.filehashes except select * from %s.filehashes;" %(sourceSchema, compSchema)
         cur.execute( command )
+        missingInComp = cur.fetchall()
+        # print("Missing files in comparison location: %s" % missingInComp)
+        __collectMissingFiles__(missingInComp, MISSING_FROM_COMP_FOLDER)
+
+        # identify missing files from the source location
+        command = "SELECT * from %s.filehashes except select * from %s.filehashes;" % (compSchema, sourceSchema)
+        cur.execute(command)
         missingInSource = cur.fetchall()
-        print("Missing files in source location: %s" %missingInSource)
+        # print("Missing files in source location: %s" % missingInSource)
+        __collectMissingFiles__(missingInSource, MISSING_FROM_SRC_FOLDER)
+
     finally:
         cur.close()
         # conn.close
         db.returnConnection(conn)
+    print("Done processing missing files.  Please look at %s and %s folders for missing files." % (MISSING_FROM_SRC_FOLDER, MISSING_FROM_COMP_FOLDER))
     return;
+
+#######################
+# Utility for copying all of the missing files from the specified result-set into the specified folder.
+#######################
+def __collectMissingFiles__( missingFiles, folderName ):
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
+
+    for missingFile in missingFiles:
+        copy( missingFile[0],  folderName)
